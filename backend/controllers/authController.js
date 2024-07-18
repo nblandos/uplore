aa113a320b3b108baff6c7bc1b03cf4d96627413
nblandos <nblandos@gmail.com>
2024-07-18 19:27:29 +0100

    boilerplate setup

---
+++ backend/controllers/authController.js
@@ -0,0 +1,17 @@
+export const login = async (req, res) => {
+  res.json({
+    message: "You hit the login endpoint",
+  });
+};
+
+export const logout = async (req, res) => {
+  res.json({
+    message: "You hit the logout endpoint",
+  });
+};
+
+export const signup = async (req, res) => {
+  res.json({
+    message: "You hit the signup endpoint",
+  });
+};
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/login", login);
+
+router.get("/login", login);
+
+router.get("/signup", signup);
+
+export default router;
